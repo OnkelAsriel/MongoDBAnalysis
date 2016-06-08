7a09f6166e2b65397d607499544dad32812f4326
OnkelAsriel <SgtBurden@gmail.com>
2016-06-09 01:12:18 +0200

    Added user+password support to ClientSetUp.py

--- ClientSetUp.py
+++ ClientSetUp.py
@@ -4,6 +4,13 @@ import pymongo
 from pymongo import MongoClient
 
 client = MongoClient()
+
+user = ""
+password = ""
+
+if user != "" and password != "":
+    client.the_database.authenticate(user, password, mechanism='SCRAM-SHA-1')
+
 db = client.database_names()
 db = client.get_database('exodef')
 objects = db.objects
