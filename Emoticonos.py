21c0f77dce9143b56bbddeef8063016d8d8cd7e7
OnkelAsriel <SgtBurden@gmail.com>
2016-06-08 14:03:02 +0200

    test

--- Emoticonos.py
+++ Emoticonos.py
@@ -1,8 +1,10 @@
 from ClientSetUp import *
 #from ROOT import TCanvas, TH1F, TTree, TFile, std
 from unidecode import unidecode
+from MakeJSON import MakeJSON
 import string
 import time
+import pickle
 
 timestart = time.time()
 print "Starting"
@@ -25,15 +27,13 @@ postn = 0
 postlimit = 0 #no limit
 print "Number of posts: " + str(obf.count())
 for post in obf:
-    tid = post.get("tid")
-    if tid in floodlist:
+    if post.get("tid") in floodlist:
         continue
     content = post.get("content")
     emojin = 0 #I could have done a dic, I chose to do this instead
-    for emoji in emojilist:
+    for emojin, emoji in enumerate(emojilist):
         if emoji[0] in content:
             messagecount[emojin] += 1
-        emojin += 1
     postn += 1
     if postlimit > 0 and postn > postlimit:
         break
@@ -43,6 +43,15 @@ for i in range(0, len(emojilist)):
 
 emojilist = sorted(emojilist, key=lambda emojilist: emojilist[1], reverse=True)
 
+print "Making JSON for exo.do:"
+
+tojson = MakeJSON(emojilist)
+with open('index.json', 'w') as f:
+    f.write(tojson)
+
+with open("emojilist.pickle", "w") as sf:
+    pickle.dump(emojilist, sf)
+    
 print "End"
 
 timeend = time.time()
